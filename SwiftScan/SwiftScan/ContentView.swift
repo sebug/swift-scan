22021b9bbc491fc65306622485d340d86836ef2a
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-11-21 11:52:21 +0100

    Adding basic project

---
+++ SwiftScan/SwiftScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftScan
+//
+//  Created by Sebastian Gfeller on 21.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftScan/SwiftScan/SwiftScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScanApp.swift
+//  SwiftScan
+//
+//  Created by Sebastian Gfeller on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
